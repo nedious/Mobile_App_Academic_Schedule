873061e7159a6c4bfa533071f1f8265a7d2ee483
nedious <ned.imhoff@gmail.com>
2023-10-07 15:38:04 -0400

    all of layout up to date

--- app/src/main/java/com/example/termv30/ui/MainActivity.java
+++ app/src/main/java/com/example/termv30/ui/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.termv30;
+package com.example.termv30.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.termv30.R;
 
 public class MainActivity extends AppCompatActivity {
 
